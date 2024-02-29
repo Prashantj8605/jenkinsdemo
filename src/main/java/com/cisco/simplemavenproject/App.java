4641b150ed38d801d0b1f446445e77926ad175db
prashjad <prashjad@cisco.com>
2024-02-29 11:47:39 +0530

    first commit

---
+++ src/main/java/com/cisco/simplemavenproject/App.java
@@ -0,0 +1,14 @@
+package com.cisco.simplemavenproject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Welcome to the World of Jenkins!" );
+        System.out.println( "Hello From my simple project" );
+    }
+}
